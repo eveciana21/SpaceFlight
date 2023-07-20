c3ebd7ab13a32bcaa952b99d2eae7fb9ffb2ac25
Eric Veciana <eveciana21@gmail.com>
2023-07-19 20:17:06 -0600

    just about finished the cockpit. Added switch cameras

---
+++ Assets/Scripts/ChangeCamera.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using Cinemachine;
+
+public class ChangeCamera : MonoBehaviour
+{
+    [SerializeField] private GameObject _cockpit;
+    //[SerializeField] private PlayableDirector _director;
+
+    [SerializeField] private GameObject _cockpitCam, _spaceShipCam;
+
+    [SerializeField] private bool _isCockpitCam;
+    void Start()
+    {
+        _cockpitCam.SetActive(false);
+        _cockpit.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (_isCockpitCam == false)
+            {
+                _cockpitCam.SetActive(true);
+                _cockpit.SetActive(true);
+                _spaceShipCam.SetActive(false);
+                _isCockpitCam = true;
+            }
+            else
+            {
+                _cockpitCam.SetActive(false);
+                _cockpit.SetActive(false);
+                _spaceShipCam.SetActive(true);
+                _isCockpitCam = false;
+            }
+        }
+    }
+}
